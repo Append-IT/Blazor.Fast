e1452bd463b884155d1fdd32d4a0279cc73361f7
Vertonghenb <ben.vertonghen@gmail.com>
2020-10-21 01:30:35 +0200

    Flipper

---
+++ source/Append.Blazor.Fast/Components/Flipper.cs
@@ -0,0 +1,20 @@
+﻿using Append.Blazor.Fast.Core;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace Append.Blazor.Fast.Components
+{
+    /// <summary>
+    /// The <see cref="Flipper"/> component is most often used to page through blocks of content or collections of ui elements.
+    /// </summary>
+    public class Flipper : FastComponent
+    {
+        /// <inheritdoc />
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            builder.OpenElement(0, $"{ThemeName}-flipper");
+            builder.AddMultipleAttributes(1, AdditionalAttributes);
+            builder.AddContent(2, ChildContent);
+            builder.CloseElement();
+        }
+    }
+}
