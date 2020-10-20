c1cb7855911cf69202f60a629c1bdbc1db4437ab
Vertonghenb <ben.vertonghen@gmail.com>
2020-10-21 01:00:43 +0200

    Accordion

---
+++ source/Append.Blazor.Fast/Components/Accordion.cs
@@ -0,0 +1,22 @@
+﻿using Append.Blazor.Fast.Core;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace Append.Blazor.Fast.Components
+{
+    /// <summary>
+    /// An <see cref="Accordion"/> is a vertically stacked set of interactive headings that each contain a title, content snippet, or thumbnail representing a section of content. 
+    /// The headings function as controls that enable users to reveal or hide their associated sections of content.
+    /// Accordions are commonly used to reduce the need to scroll when presenting multiple sections of content on a single page.
+    /// </summary>
+    public class Accordion : FastComponent
+    {
+        /// <inheritdoc />
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            builder.OpenElement(0, $"{ThemeName}-accordion");
+            builder.AddMultipleAttributes(1, AdditionalAttributes);
+            builder.AddContent(2, ChildContent);
+            builder.CloseElement();
+        }
+    }
+}
---
+++ source/Append.Blazor.Fast/Components/AccordionItem.cs
@@ -0,0 +1,20 @@
+﻿using Append.Blazor.Fast.Core;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace Append.Blazor.Fast.Components
+{
+    /// <summary>
+    /// The <see cref="AccordionItem"/> should always be placed inside a <see cref="Accordion"/>.
+    /// </summary>
+    public class AccordionItem : FastComponent
+    {
+        /// <inheritdoc />
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            builder.OpenElement(0, $"{ThemeName}-accordion-item");
+            builder.AddMultipleAttributes(1, AdditionalAttributes);
+            builder.AddContent(2, ChildContent);
+            builder.CloseElement();
+        }
+    }
+}
