5e4d9b31de6ba5fcbc950ddc5fdad46ac4d3c635
Vertonghenb <ben.vertonghen@gmail.com>
2020-10-21 03:11:51 +0200

    Skeleton

---
+++ source/Append.Blazor.Fast/Components/Skeleton.cs
@@ -0,0 +1,20 @@
+﻿using Append.Blazor.Fast.Core;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace Append.Blazor.Fast.Components
+{
+    /// <summary>
+    /// A <see cref="Skeleton"/> is used as a visual placeholder for an element while it is in a loading state and usually presents itself as a simplified wireframe-like version of the UI it is representing.
+    /// </summary>
+    public class Skeleton : FastComponent
+    {
+        /// <inheritdoc />
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            builder.OpenElement(0, $"{ThemeName}-skeleton");
+            builder.AddMultipleAttributes(1, AdditionalAttributes);
+            builder.AddContent(2, ChildContent);
+            builder.CloseElement();
+        }
+    }
+}
