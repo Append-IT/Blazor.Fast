e4e1a4e7e6cde714568d72b45acda256278c808f
Vertonghenb <ben.vertonghen@gmail.com>
2020-10-21 23:28:23 +0200

    TextArea

---
+++ source/Append.Blazor.Fast/Components/TextArea.cs
@@ -0,0 +1,32 @@
+﻿using Append.Blazor.Fast.Core;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Append.Blazor.Fast.Components
+{
+    /// <summary>
+    /// A multiline input component for editing <see cref="string"/> values.
+    /// </summary>
+    public class TextArea : FastInputComponent<string>
+    {
+        /// <inheritdoc />
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            builder.OpenElement(0, $"{ThemeName}-text-area");
+            builder.AddMultipleAttributes(1, AdditionalAttributes);
+            builder.AddAttribute(3, "class", CssClass);
+            builder.AddAttribute(4, "value", BindConverter.FormatValue(CurrentValue));
+            builder.AddAttribute(5, "onchange", EventCallback.Factory.CreateBinder<string?>(this, __value => CurrentValueAsString = __value, CurrentValueAsString));
+            builder.AddContent(6, ChildContent);
+            builder.CloseElement();
+        }
+        /// <inheritdoc />
+        protected override bool TryParseValueFromString(string? value, out string? result, [NotNullWhen(false)] out string? validationErrorMessage)
+        {
+            result = value;
+            validationErrorMessage = null;
+            return true;
+        }
+    }
+}
