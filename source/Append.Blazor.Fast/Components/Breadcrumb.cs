90764a9f471286df2d80bdaec4fb69a79dc6f24a
Vertonghenb <ben.vertonghen@gmail.com>
2020-10-28 12:33:54 +0100

    [Component] Breadcrumb
    Adds the breadcrumb and breadcrumb item.
    Fixes #5

---
+++ source/Append.Blazor.Fast/Components/Breadcrumb.cs
@@ -0,0 +1,20 @@
+﻿using Append.Blazor.Fast.Core;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace Append.Blazor.Fast.Components
+{
+    /// <summary>
+    /// A <see cref="Breadcrumb"/> component is used as a navigational aid, allowing users to maintain awareness of their locations within a program, app, or a website.
+    /// </summary>
+    public class Breadcrumb : FastComponent
+    {
+        /// <inheritdoc />
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            builder.OpenElement(0, $"{ThemeName}-breadcrumb");
+            builder.AddMultipleAttributes(1, AdditionalAttributes);
+            builder.AddContent(2, ChildContent);
+            builder.CloseElement();
+        }
+    }
+}
